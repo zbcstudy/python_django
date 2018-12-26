2524dd3128f77abd89083f0ff653165e53da12aa
Hasee <1434756304@qq.com>
2018-12-26 22:52:43 +0800

    基本页面学习

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('第一个视图')
+
+
+def detail(request,num):
+    return HttpResponse('detail-%s' % num)
+
